395f5dbe23a87c838ff5edb1d8d75c221d2a7a10
julianfiall <44555913+julianfiall@users.noreply.github.com>
2020-04-15 22:13:23 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    bankbal()
+    function bankbal(){
+        document.getElementById("bankbal").innerHTML = "$" + localStorage.bank;
+    };
+}
+function work() {
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.bank) {
+            localStorage.bank = Number(localStorage.bank) + 200;
+        } else {
+            localStorage.bank = 0;
+        }
+        document.getElementById("bankbal").innerHTML = "$" + localStorage.bank;
+    } else {
+        document.getElementById("bankbal").innerHTML = "Your browser does not support this game.";
+    }
+    bank = bank + 200;
+    document.getElementById('bankbal').innerHTML = bank;
+    localStorage.setItem(bank);
+}
